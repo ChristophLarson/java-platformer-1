b41fd3917a007d46e3dcf049156ca7a45a43ee6b
Christoph Larson <47951382+ChristophLarson@users.noreply.github.com>
2023-04-02 21:01:27 -0500

    Add keyboard inputs for four-d mvmt.

--- FirstGame/src/main/GamePanel.java
+++ FirstGame/src/main/GamePanel.java
@@ -4,16 +4,41 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 public class GamePanel extends JPanel{
+	
+	private MouseInputs mouseInputs;
+	
+	//Control rectangle position
+	private int xDelta = 0, yDelta = 0;
 	
 	public GamePanel() {
 		
+		mouseInputs = new MouseInputs();
+		
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+	}
+	
+	public void ChangeXDelta(int delta) {
+		
+		this.xDelta += delta;
+		repaint();
+	}
+	
+	public void ChangeYDelta(int delta) {
+		
+		this.yDelta += delta;
+		repaint();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.fillRect(100, 100, 200, 50);
+		g.fillRect(100 + xDelta, 100 + yDelta, 200, 50);
 	}
 
 }
