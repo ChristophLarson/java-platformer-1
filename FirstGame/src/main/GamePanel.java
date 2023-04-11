8827356939a618b43940bb244896e55e99bf6685
Christoph Larson <47951382+ChristophLarson@users.noreply.github.com>
2023-04-11 10:15:42 -0500

    Import image and control as sprite

--- FirstGame/src/main/GamePanel.java
+++ FirstGame/src/main/GamePanel.java
@@ -1,6 +1,11 @@
 package main;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 //import java.awt.Toolkit;
 import javax.swing.JPanel;
 import java.awt.Dimension;
@@ -15,18 +20,31 @@ public class GamePanel extends JPanel{
 	//Control rectangle position
 	private float xDelta = 100, yDelta = 100;
 	
-	//Fps counter
-	private int frames = 0;
-	private long lastCheck = 0;
+	private BufferedImage img, subImg;
 	
 	public GamePanel() {
 		mouseInputs = new MouseInputs(this);
+		
+		importImg();
+		
 		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 	}
 	
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+		
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
 	private void setPanelSize() {
 		Dimension size = new Dimension(1280, 800);
 		setPreferredSize(size);
@@ -54,7 +72,8 @@ public class GamePanel extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		
+		subImg = img.getSubimage(1*64, 8*40, 64, 40);
+		g.drawImage(subImg, (int)xDelta, (int)yDelta, 128, 80, null);
 	}
 	
 
