6f2e550f50dc7b4161afc23028b8744c7276564a
Christoph Larson <47951382+ChristophLarson@users.noreply.github.com>
2023-04-15 22:22:30 -0500

    Load in level one

--- FirstGame/src/levels/LevelManager.java
+++ FirstGame/src/levels/LevelManager.java
@@ -10,14 +10,17 @@ public class LevelManager {
 
 	private Game game;
 	private BufferedImage[] levelSprite;
+	private Level levelOne;
 
-	private final int OUTSIDE_GRID_WIDTH = 12;
-	private final int OUTSIDE_GRID_HEIGHT = 4;
+	public static final int OUTSIDE_GRID_WIDTH = 12;
+	public static final int OUTSIDE_GRID_HEIGHT = 4;
 
 	public LevelManager(Game game) {
 		this.game = game;
 		// levelSprite = LoadSave.getSpriteAtlas(LoadSave.LEVEL_ATLAS);
 		importOutsideSprites();
+		levelOne = new Level(LoadSave.getLevelData());
+
 	}
 
 	private void importOutsideSprites() {
@@ -27,14 +30,26 @@ public class LevelManager {
 		for (int j = 0; j < OUTSIDE_GRID_HEIGHT; j++) {
 			for (int i = 0; i < OUTSIDE_GRID_WIDTH; i++) {
 				int index = j * OUTSIDE_GRID_WIDTH + i;
-				levelSprite[index] = img.getSubimage(i * 32, j * 32, 32, 32);
+				levelSprite[index] = img.getSubimage(
+						i * Game.TILES_DEFAULT_SIZE,
+						j * Game.TILES_DEFAULT_SIZE, Game.TILES_DEFAULT_SIZE,
+						Game.TILES_DEFAULT_SIZE);
 			}
 		}
 
 	}
 
 	public void draw(Graphics g) {
-		g.drawImage(levelSprite[2], 0, 0, null);
+		for (int j = 0; j < Game.TILES_IN_HEIGHT; j++) {
+			for (int i = 0; i < Game.TILES_IN_WIDTH; i++) {
+				int index = levelOne.getSpriteIndex(i, j);
+				g.drawImage(levelSprite[index], Game.TILES_SIZE * i,
+						Game.TILES_SIZE * j, Game.TILES_SIZE, Game.TILES_SIZE,
+						null);
+
+			}
+		}
+
 	}
 
 	public void update() {
